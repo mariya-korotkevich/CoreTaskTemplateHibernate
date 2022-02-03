cf21e4f9706101511ae966f4b97c629852bb0831
Mariya <maria.korotkevich@yandex.ru>
2022-02-03 20:13:16 +0300

    изменен тип ссылки на тип интерфейса

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,13 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Маша", "Иванова", (byte) 18);
         userService.saveUser("Даша", "Петрова", (byte) 6);
