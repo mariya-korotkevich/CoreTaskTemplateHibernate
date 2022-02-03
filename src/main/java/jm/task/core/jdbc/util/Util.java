e6555b51f7aa9fbd6b80e3e79f556b136f039ed5
Mariya <maria.korotkevich@yandex.ru>
2022-02-03 20:14:05 +0300

    добавлены строковые константы

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,10 +6,11 @@ import java.sql.SQLException;
 
 public class Util {
 
+    private static final String userName = "root";
+    private static final String password = "1234";
+    private static final String connectionURL = "jdbc:mysql://localhost:3306/myBase?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
+
     public static Connection getConnection() throws SQLException {
-        String userName = "root";
-        String password = "1234";
-        String connectionURL = "jdbc:mysql://localhost:3306/myBase?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
         return DriverManager.getConnection(connectionURL, userName, password);
     }
 }
