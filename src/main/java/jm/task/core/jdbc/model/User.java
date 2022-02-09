75b3d4e3c1a4beb45bcb921ab32518aa3ab82c07
Mariya <maria.korotkevich@yandex.ru>
2022-02-09 20:38:02 +0300

    добавлены аннотации @Entity и @GeneratedValue задан параметр name в аннотации @Table

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -3,10 +3,15 @@ package jm.task.core.jdbc.model;
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
-@Table
+@Entity
+@Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
